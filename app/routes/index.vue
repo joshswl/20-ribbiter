7a27d6c14908b3d911849e6915bdc929a23022f0
Josh Lee <joshswl@gmail.com>
2017-03-16 17:25:46 -0500

    progress in styling welcome pg

--- app/routes/index.vue
+++ app/routes/index.vue
@@ -1,11 +1,24 @@
 <template lang="html">
   <div class="">
-
+    <div class="index">
+      <div class="container">
+        <h2 class="welcome">Ribbiter Welcomes You</h2>
+        <div class="buttons">
+          <button class="buttons__actual">
+            <a href="#">Register</a>
+          </button>
+          <button class="buttons__actual">
+            <a href="#">Login</a>
+          </button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
+  name: 'Index',
   data() {
     return {
     };
